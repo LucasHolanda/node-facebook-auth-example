7c65b39767d6306dabe0d0751c0faca142819c80
Lucas Holanda <luckas.holanda@gmail.com>
2020-12-30 15:47:00 -0400

    Ultimas alterações realizadas

---
+++ configuration/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  /*
+   * This file contains the configurations information of Twitter login app.
+   * It consists of Twitter app information, database information.
+   */
+
+  facebook_api_key: "YOUR_API_KEY",
+  facebook_api_secret: "YOUR_API_SECRET",
+  callback_url: "http://localhost:3333/auth/facebook/callback",
+  use_database: false,
+  host: "localhost",
+  username: "root",
+  password: "",
+  database: ""
+};
